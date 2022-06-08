95e0fa3fc0dc6f8518d981a0a458c593f524fe51
Víctor Padrón <victorpadronolivas@gmail.com>
2022-06-08 05:59:43 -0600

    added heroku port

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN npm install
 RUN npm run build
 
 
-CMD ["npm", "run", "start"]
+CMD ["npm", "run", "start", "--bind", "0.0.0.0:$PORT", "wsgi"]
--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:3001",
 });
 
 export default axiosInstance;
