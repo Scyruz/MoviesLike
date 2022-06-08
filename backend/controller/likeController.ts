f0c732f088c7173e38169eecab591ef74803cf50
GerardoGran <gerardogr.al@outlook.com>
2022-06-08 00:51:20 -0600

    like route implemmented

--- backend/controller/likeController.ts
+++ backend/controller/likeController.ts
@@ -1,8 +1,44 @@
 import { Request, Response } from "express";
+import { getMovieList } from "../utils/getMoviesByTitle"
 
 export const likeController: Function = async (req: Request, res: Response) => {
-  const { movieId, movieName } = req.query;
+  const movieName = req.query.q!;
   // make request to python websocket using movieName
-  console.log(movieId, movieName);
-  return res.status(200);
+  // Websocket to call model.py
+  const WebSocketClient = require('websocket').client;
+  var client = new WebSocketClient();
+
+  client.on('connectFailed', function (error: Error) {
+    console.log('Connect Error: ' + error.message);
+  });
+
+  client.on('connect', function (connection: any) {
+    connection.on('error', function (error: Error) {
+      console.log("Connection Error: " + error.message);
+    });
+    connection.on('close', function () {
+      console.log('WebSocket Connection Closed');
+    });
+    connection.on('message', async (message: any) => {
+      if (message.type === 'utf8') {
+        const data = JSON.parse(message.utf8Data)
+        console.log("Received: '" + data + "'");
+
+        const movieList = await getMovieList(data.recommendation_titles)
+        return res.json(movieList)
+
+        // return res.json(JSON.parse(data))
+      }
+    });
+
+    function sendTitle(title: any) {
+      if (connection.connected) {
+        connection.sendUTF(title);
+        setTimeout(sendTitle, 1000);
+      }
+    }
+    sendTitle(movieName);
+  });
+
+  client.connect('ws://localhost:8765/');
 };
--- backend/utils/getMoviesByTitle.ts
+++ backend/utils/getMoviesByTitle.ts
@@ -7,3 +7,22 @@ const axios = require("axios");
 
 const IMDB_API_KEY = config.IMDB_API_KEY;
 const baseURL = `https://imdb-api.com/en/API/SearchMovie/${IMDB_API_KEY}/`;
+
+const getMovieList = async (titleObjects: Array<any>) => {
+    let movieList: Array<MovieInterface> = [];
+    for (let { title } of titleObjects) {
+        try {
+            let data = await axios.get(baseURL + title);
+            if (data.data.errorMessage !== "") {
+                return [];
+            }
+            console.log(data.data.results[0])
+            movieList.push(data.data.results[0]);
+        } catch (error) {
+            return [];
+        }
+    }
+    return movieList
+}
+
+export { getMovieList }
